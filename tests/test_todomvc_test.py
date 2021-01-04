ef56c5a6c01492f5b7e5a2917110a744e78ea97e
Liubire <zlata744877@gmail.com>
2021-01-04 21:50:16 +0200

    changes after checking

--- tests/test_todomvc_test.py
+++ tests/test_todomvc_test.py
@@ -5,15 +5,12 @@ from selene.support.conditions import have
 
 def test_complete_todo():
     browser.open('http://todomvc.com/examples/emberjs/')
+
     s('#new-todo').type('a').press_enter()
     s('#new-todo').type('b').press_enter()
     s('#new-todo').type('c').press_enter()
     ss('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
     s('#todo-list>li:nth-of-type(2) .toggle').click()
-
-    ss('#todo-list>li .completed').should(have.exact_text('b'))
+    ss('#todo-list>li.completed').should(have.exact_texts('b'))
     ss('#todo-list>li:not(.completed)').should(have.exact_texts('a', 'c'))
-
-
-
